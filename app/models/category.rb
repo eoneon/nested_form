b538f7ed5be28fb47f4edd31f454b75ad04dc0a4
Eon <eoneon123@gmail.com>
2017-06-23 17:09:46 -0700

    deleted rest of mounings

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,8 @@
 class Category < ActiveRecord::Base
   belongs_to :item
-<<<<<<< HEAD
-  belongs_to :mounting
-=======
   belongs_to :description
 
   # def self.model_name
   #   ActiveModel::Name.new(self, nil, "Category")
   # end
->>>>>>> hmt_types
 end
--- app/models/mounting.rb
+++
@@ -1,4 +0,0 @@
-class Mounting < ActiveRecord::Base
-  has_many :categories
-  has_many :items, through: :categories
-end
